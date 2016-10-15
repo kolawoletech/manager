ff2ec62445729620884c59b5c8afd1350f2a4137
mpcen <vccengineers12@gmail.com>
2016-10-15 15:36:37 -0700

    first commit. onto Routing

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+import LoginForm from './components/LoginForm';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk, logger())(createStore);
+
+class App extends Component {
+	componentWillMount() {
+		const config = {
+			apiKey: 'AIzaSyB1K56AP4LzmxkYpg1qPq2CfhM0HPiZQlo',
+			authDomain: 'manager-cb77a.firebaseapp.com',
+			databaseURL: 'https://manager-cb77a.firebaseio.com',
+			storageBucket: 'manager-cb77a.appspot.com',
+			messagingSenderId: '1037526763592'
+		};
+
+		firebase.initializeApp(config);
+	}
+
+	render() {
+		return (
+			<Provider store={createStoreWithMiddleware(reducers)}>
+				<LoginForm />
+			</Provider>
+		);
+	}
+}
+
+export default App;
